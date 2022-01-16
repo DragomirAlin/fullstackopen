b4f8609b23dfc947a94d1e4ca7bbcc344535ca5e
Dragomir Alin <dragomirdanielalin@gmail.com>
2022-01-16 18:00:14 +0200

    add unicafe project

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -71,7 +71,7 @@ const Order = () => {
         const newCappuccino = {
             // espresso: orders.espresso,
             ...orders,
-            cappuccino: orders.cappuccino + 1,
+            cappuccino: orders.cappuccino++,
         }
         setOrders(newCappuccino);
     }
@@ -86,11 +86,53 @@ const Order = () => {
     )
 }
 
+const Tracking = () => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+    }
+
+    return (
+        <div>
+            {left}
+            <button onClick={handleLeftClick}>left</button>
+            <button onClick={handleRightClick}>right</button>
+            {right}
+            <History allClicks={allClicks} />
+
+        </div>
+    )
+}
+
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
+    }
+    return (
+        <div>
+            button press history: {props.allClicks.join(' ')}
+        </div>
+    )
+}
+
 const App = () => {
 
     return (
         <div>
-            <Order/>
+            <Tracking/>
         </div>
     )
 }
