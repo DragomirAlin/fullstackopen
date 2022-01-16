7da01a61ea793ebc5202291c747c4f61648e4468
Dragomir Alin <dragomirdanielalin@gmail.com>
2022-01-16 16:38:08 +0200

    try complex state

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -32,23 +32,65 @@ const Hello = (props) => {
     )
 }
 
+const Button = ({onClick, text}) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
 
-const App = (props) => {
-    const [counter, setCounter] = useState(0)
-
-    setTimeout(
-        () => setCounter(counter + 1),
-        1000
-    )
-    console.log('rendering...', counter)
-
+const Counters = () => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
 
     return (
         <div>
-            {counter}
+            {left}
+            <Button text={"left1"} onClick={() => setLeft(left + 1)}/>
+            <Button text={"right1"} onClick={() => setRight(right + 1)}/>
 
-            <div>Hello World</div>
+            {right}
+        </div>
+    )
+}
 
+const Order = () => {
+    const [orders, setOrders] = useState({
+        espresso: 0, cappuccino: 0
+    })
+
+    const addEspresso = () => {
+        const newEspresso = {
+            ...orders,
+            espresso: orders.espresso + 1,
+            // cappuccino: orders.cappuccino,
+        }
+        setOrders(newEspresso);
+    }
+
+    const addCappuccino = () => {
+        const newCappuccino = {
+            // espresso: orders.espresso,
+            ...orders,
+            cappuccino: orders.cappuccino + 1,
+        }
+        setOrders(newCappuccino);
+    }
+
+    return (
+        <div>
+            {orders.espresso}
+            <Button text={"espresso"} onClick={addEspresso}/>
+            <Button text={"cappuccino"} onClick={addCappuccino}/>
+            {orders.cappuccino}
+        </div>
+    )
+}
+
+const App = () => {
+
+    return (
+        <div>
+            <Order/>
         </div>
     )
 }
